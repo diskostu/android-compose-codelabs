2d29b1edb597bbb15dfa7bc24089adedbb6fa8df
Denny Kluge <denny.kluge@gmail.com>
2024-01-24 21:39:37 +0100

    finished "LaunchedEffect"

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -20,18 +20,35 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
 @Composable
 fun LandingScreen(onTimeout: () -> Unit, modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        // TODO Codelab: LaunchedEffect and rememberUpdatedState step
-        // TODO: Make LandingScreen disappear after loading data
+        // onTimeout may change while the side effect (LaunchedEffect coroutine) is still in
+        // progress. In this case, using onTimeout() in the coroutine would perhaps use an outdated
+        // lambda.
+        // To always have the newest "version" of onTimeout in the coroutine scope, we update it
+        // with rememberUpdatedState.
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+        // DONE Codelab: LaunchedEffect and rememberUpdatedState step
+        // DONE: Make LandingScreen disappear after loading data
+
+        LaunchedEffect(Unit) {
+            delay(SplashWaitTime) // Simulates loading things
+            currentOnTimeout()
+        }
+
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
