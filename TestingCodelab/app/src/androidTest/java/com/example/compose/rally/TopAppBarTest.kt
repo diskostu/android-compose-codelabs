1e1cded7b63a3315dbc0fc85157055a8c4982788
Denny Kluge <denny.kluge@gmail.com>
2024-05-03 23:04:57 +0200

    finished "merged and unmerged semantic trees"

--- TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,6 +1,10 @@
 package com.example.compose.rally
 
 import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.hasAnyDescendant
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
@@ -47,11 +51,10 @@ class TopAppBarTest {
             )
         }
 
-        composeTestRule.onRoot().printToLog("currentLabelExists")
+        val parentMatcher = hasContentDescription(RallyScreen.Accounts.name)
+        val childMatcher = hasText(RallyScreen.Accounts.name.uppercase())
+        val matcher = hasParent(parentMatcher).and(childMatcher)
 
-        composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertExists()
+        composeTestRule.onNode(matcher, useUnmergedTree = true).assertExists()
     }
-
 }
